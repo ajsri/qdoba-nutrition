ffaf9bec4f99934ae3a8c16c3ba97d2a452c90a6
AJ Srivastava <anuj@four7.us>
2016-04-17 10:43:04 -0500

    add index - transpile to bundle - no need for express atm

---
+++ README.md
@@ -0,0 +1,2 @@
+why did you take away the nutrition calculator qdoba?
+I used it. A lot.
--- app/index.js
+++ app/index.js
@@ -1,5 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-console.log(React);
+class QdobaNutrition extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      title: "Qdoba Nutrition"
+    }
+  }
+  render(){
+    return(
+      <div>{this.state.title}</div>
+    )
+  }
+}
+
+ReactDOM.render(<QdobaNutrition/>, document.getElementById('app'));
 
