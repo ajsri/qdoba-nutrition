0b0178410b53a47575d15bac83b2fe160d3fba35
AJ Srivastava <aj.srivastava@ctl.io>
2016-08-01 12:56:12 -0500

    fix port issue

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 })
 
-app.listen(4741, function(){
-  console.log("Listening on port 4741")
+app.listen(process.env.PORT || 4741), function(){
+  console.log("Listening")
 })
